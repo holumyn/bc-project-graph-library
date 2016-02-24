4435468c74e17195a1728fe0dfed01cf03fcd7e9
holumyn <noble2016@gmail.com>
2016-02-24 12:50:11 +0100

    Graph Library

--- graph_lib.py
+++ graph_lib.py
@@ -1,15 +1,27 @@
 class Graph_lib(object):
 	"""
-	Graph Library
+	Graph Library that handles traversals,
+	both Depth First Search and Breath First Search 
 	"""
 	def __init__(self):
 		pass
 	
-	def dfs(self):
-		pass
+	def dfs(self,graph,start):
+		visited = []
+		stack = [start]
+
+		while stack:
+			node = stack.pop()
+
+			if node not in visited:
+				visited.append(node)
+				stack.extend([x for x in graph[node] if x not in visited])
+
+		return visited
 	
 	def bfs(self):
 		pass
+
 		
 
 	
